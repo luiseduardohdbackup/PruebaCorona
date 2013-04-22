f29950748a652c264f8ff6d382339c7ca7c76060
Daniel Sánchez Seijo <daniseijo12@gmail.com>
2013-04-23 00:54:39 +0200

    Background estatico

--- main.lua
+++ main.lua
@@ -4,4 +4,14 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local backgroung = display.newImage("bg.png")
+
+local city1 = display.newImage("city1.png")
+city1:setReferencePoint(display.BottomLeftReferencePoint)
+city1.x = 0
+city1.y = 320
+
+local city2 = display.newImage("city2.png")
+city2:setReferencePoint(display.BottomLeftReferencePoint)
+city2.x = 0
+city2.y = 320
